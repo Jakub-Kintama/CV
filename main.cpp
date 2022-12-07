f99b49b4c8c569973b3d4216504c7400e3a8972b
Jakub-kintama <kubaisnow@gmail.com>
2022-12-07 16:29:54 +0100

    hsv working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <opencv2/imgproc.hpp>
 
 int main(int argc, char** argv)
 {
@@ -9,15 +10,58 @@ int main(int argc, char** argv)
     if (!vc.isOpened() ) return 1;
     cv::Mat frame;
 
-    cv::namedWindow("Window", WINDOW_AUTOSIZE); // Create a window for display.
-    int i = 0;
-    while (waitKey(10) != 27) {
-        vc >> frame;
-	    cv::Mat src = frame;
-	    cv::Mat dst;
-	    cv::flip(src, dst, 1);
-        cv::imshow("Frame", dst);
+    namedWindow("Control", WINDOW_AUTOSIZE); //create a window called "Control"
 
+    int iLowH = 0;
+    int iHighH = 179;
+    int iLowS = 0;
+    int iHighS = 255;
+    int iLowV = 0;
+    int iHighV = 255;
+    createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    createTrackbar("HighH", "Control", &iHighH, 179);
+    createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    createTrackbar("HighS", "Control", &iHighS, 255);
+    createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    createTrackbar("HighV", "Control", &iHighV, 255);
+
+    namedWindow("Window", WINDOW_AUTOSIZE); // Create a window for display.
+
+    while (waitKey(10) != 27) {
+        Mat imgOriginal;
+
+        bool bSuccess = vc.read(imgOriginal); // read a new frame from video
+
+        if (!bSuccess) //if not success, break loop
+        {
+            std::cout << "Cannot read a frame from video stream" << std::endl;
+            break;
+        }
+
+        Mat imgHSV;
+
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+        Mat imgThresholded;
+
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+        //morphological opening (remove small objects from the foreground)
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+        //morphological closing (fill small holes in the foreground)
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+        imshow("Thresholded Image", imgThresholded); //show the thresholded image
+        imshow("Original", imgOriginal); //show the original image
+
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+        {
+            std::cout << "esc key is pressed by user" << std::endl;
+            break;
+        }
     }
     return 0;
 }
